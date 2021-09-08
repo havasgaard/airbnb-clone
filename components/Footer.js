2be1a1c4f8dfa85e5a6f7bed3733c4ee5e374bd7
Didrik Havasgaard <32906442+havasgaard@users.noreply.github.com>
2021-09-08 21:08:38 +0200

    day two build

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p className="cursor-pointer">How Airbnb works</p>
+                <p className="cursor-pointer">News room</p>
+                <p className="cursor-pointer">Investors</p>
+                <p className="cursor-pointer">Airbnb Plus</p>
+                <p className="cursor-pointer">Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p className="cursor-pointer">Accessability</p>
+                <p className="cursor-pointer">This is not a real site</p>
+                <p className="cursor-pointer">It's a pretty awesome clone</p>
+                <p className="cursor-pointer">Referrals accepted</p>
+                <p className="cursor-pointer">BASHAREACT</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p className="cursor-pointer">Flabasha</p>
+                <p className="cursor-pointer">Presents</p>
+                <p className="cursor-pointer">Airbnb</p>
+                <p className="cursor-pointer">Clone</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p className="cursor-pointer">Help Centre</p>
+                <p className="cursor-pointer">Trust & Safety</p>
+                <p className="cursor-pointer">Say Hello</p>
+                <p className="cursor-pointer">Over at Discord</p>
+                <p className="cursor-pointer">https://discord.gg/NHjFZTsCM8</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
