7839ea2b22d8c301f5d2b77ed915dfc54e075167
Didrik Havasgaard <32906442+havasgaard@users.noreply.github.com>
2021-09-09 19:55:12 +0200

    Day Four build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
         domains: ['link.papareact.com', 'links.papareact.com'],
     },
+    env: {
+        mapbox_key:"pk.eyJ1IjoiaGF2YXNnYWFyZCIsImEiOiJja3RidnYxODIxemlsMm9wZHp1NTZ4MG41In0.VIqvOUij9zvfCnhQhRg3Mw",
+    }
 };
